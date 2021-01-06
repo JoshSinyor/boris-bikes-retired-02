c84b36692753305eb12e0bc51eda3b3ee3371da6
Joshua Sinyor <josh.sinyor@gmail.com>
2021-01-06 15:42:01 +0000

    Created bike.rb, bike_spec.rb. Updated docking_station.rb, docking_station_spec.rb. This reflects new tests and feature testing.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,13 @@
 require 'docking_station'
 
 describe DockingStation do
-    it { is_expected.to respond_to(:release_bike) }
+
+    it { is_expected.to respond_to :release_bike }
+
+    it 'Releases a bike and expects it to be working' do
+      bike = subject.release_bike
+      expect(bike).to respond_to(:working?)
+      expect(bike.working?).to eq true
+    end
+
 end
