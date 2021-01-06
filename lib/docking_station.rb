29006b6b6ae9a6e4d92732a529b74ea346f094fc
Joshua Sinyor <josh.sinyor@gmail.com>
2021-01-06 17:35:40 +0000

    Updated docking_station.rb and docking_station_spec.rb to reflect dock_bike method.

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,22 @@
 require_relative "bike"
 
 class DockingStation
+    attr_reader :bike_counter
+
+    def initialize
+      @bike_counter = 0
+    end
+
     def release_bike
       Bike.new
+    end
+
+    def dock_bike(bike)
+      if !bike.nil?
+        @bike_counter += 1
+        true
+      else
+        false
+      end
     end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,4 +10,23 @@ describe DockingStation do
       expect(bike.working?).to eq true
     end
 
+    it { is_expected.to respond_to :dock_bike }
+
+    it 'A valid bike returned' do
+      # bike = Bike.new
+      expect(subject.dock_bike(bike)).to eq true
+    end
+
+    it 'Has bikes' do
+      # bike = Bike.new
+      subject.dock_bike(bike)
+      expect(subject.bike_counter).to be > 0
+    end
+
 end
+
+=begin
+As a member of the public
+So I can decide whether to use the docking station
+I want to see a bike that has been docked
+=end
